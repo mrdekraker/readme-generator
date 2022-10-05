8e8600fb40143d7fbdea3a4413d20cbd5cdc695b
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-10-05 18:25:33 -0500

    added requires and first questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,37 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: `input`,
+    name: `name`,
+    message: `Welcome to the README Generator! Please enter your full name?`,
+    validate: (nameResponse) => {
+      if (nameResponse) {
+        return true;
+      }
+      console.log(`Be proud of your work! Please enter your full name!`);
+      return false;
+    },
+  },
+  {
+    type: `input`,
+    name: `github`,
+    message: `Please enter your GitHub username:`,
+    validate: (githubResponse) => {
+      if (githubResponse) {
+        return true;
+      }
+      console.log(
+        `Let people know where to find your work! Please enter your GitHub username!`
+      );
+      return false;
+    },
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
